814f8b16da9a6d3bcc984b0c3a2525b6eb337d16
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-01-07 04:45:58 -0800

    Do not skip SRI even if integrity attribute is cleared later
    According to the HTML spec, the integrity attribute of script
    elements are used in #prepare-a-script, and thus the changes
    to the integrity attribute after #prepare-a-script shouldn't be
    reflected.
    However, if the integrity attribute is cleared after
    #prepare-a-script, SRI check is skipped on Chromium, because whether
    the integrity attribute is empty is checked on script load finish.
    
    This CL replaces the integrity attribute check with
    ScriptFetchOptions' integrity metadata check that
    reflects the integrity metadata set at #prepare-a-script.
    
    This CL removes clear-integrity-attribute.html web test
    (expecting the old behavior) and adds WPTs expecting
    the new and spec conformant behavior and passing on
    Chrome after this CL/Firefox/Safari.
    
    Bug: 686281
    Change-Id: Ie31facd58bc3cdc3dd8056694b6da0d043c392c8

--- resources/sriharness.js
+++ resources/sriharness.js
@@ -1,17 +1,20 @@
-var SRIScriptTest = function(pass, name, src, integrityValue, crossoriginValue, nonce) {
+var SRIScriptTest = function(pass, name, src, integrityValue, crossoriginValue, nonce, integrityValueAfterPrepare) {
     this.pass = pass;
     this.name = "Script: " + name;
     this.src = src;
     this.integrityValue = integrityValue;
     this.crossoriginValue = crossoriginValue;
     this.nonce = nonce;
+    this.integrityValueAfterPrepare = integrityValueAfterPrepare;
 }
 
 SRIScriptTest.prototype.execute = function() {
     var test = async_test(this.name);
     var e = document.createElement("script");
     e.src = this.src;
-    e.setAttribute("integrity", this.integrityValue);
+    if (this.integrityValue) {
+      e.setAttribute("integrity", this.integrityValue);
+    }
     if(this.crossoriginValue) {
         e.setAttribute("crossorigin", this.crossoriginValue);
     }
@@ -30,6 +33,14 @@ SRIScriptTest.prototype.execute = function() {
        e.addEventListener("error", function() {test.done()});
     }
     document.body.appendChild(e);
+
+    // If `this.integrityValueAfterPrepare` is not set, `this.integrityValue`
+    // is kept as the integrity attribute value.
+    if (this.integrityValueAfterPrepare === null) {
+      e.removeAttribute("integrity");
+    } else if (this.integrityValueAfterPrepare) {
+      e.setAttribute("integrity", this.integrityValueAfterPrepare);
+    }
 };
 
 function set_extra_attributes(element, attrs) {
